9e06bcd33ea7e1f43a8f6ebf6b1914f103b60801
Morteza1814 <65571259+Morteza1814@users.noreply.github.com>
2022-06-16 19:10:36 -0400

    Create NaiveBitVectorFullMatcher.py
    This file includes a partial matcher that counts the number of kmers in the query that is seen in the read and if the number is equal to total number of the kmers in a query, it will assume that as a match.

---
+++ NaiveBitVectorFullMatcher.py
@@ -0,0 +1,84 @@
+import sys
+
+KMER_LENGTH = 4
+
+
+readFile = open("/bigtemp/rgq5aw/naiveBitVectorData/sampledDataFromSRR.fa", "r")
+queryFile = open("/bigtemp/rgq5aw/naiveBitVectorData/sampledQueryDataFromSRR.fa", "r")
+
+
+reads = dict()
+key=''
+value=''
+while(True):
+        line = readFile.readline()
+        if 'chr' in line and key=='':
+                 key = line[1:len(line)]
+
+        if 'chr' not in line and len(line)!=0 and key!='':
+                value = line
+
+        if key!='' and value!='':
+                reads[key] = value
+                key=''
+                value=''
+
+        if not line:
+                break
+
+i=0
+for value in reads:
+        i+=1
+print(i)
+
+queries = dict()
+key=''
+value=''
+while(True):
+        line = queryFile.readline()
+        if 'chr' in line and key=='':
+                key = line[1:len(line)]
+
+        if 'chr' not in line and len(line)!=0 and key!='':
+                value = line
+
+        if key!='' and value!='':
+                queries[key] = value
+                key=''
+                value=''
+
+        if not line:
+                break
+
+i=0
+for key in queries:
+        i+=1
+print(i)
+
+readFile.close()
+queryFile.close()
+
+numberOfBitVectorMatches=0
+numberOfTrueMatches=0
+queriesEvaluated=0
+for queryInd,query in queries.items():
+        #print("query=\n" + query)
+        for readInd, read in reads.items():
+                i=0
+                numberOfKmersFound=0
+                while i<(len(query)-KMER_LENGTH):
+                        kmer = query[i:i+KMER_LENGTH]
+                        if kmer in read:
+                                numberOfKmersFound+=1
+                        i+=1
+                if numberOfKmersFound==((len(query)-1)-KMER_LENGTH+1):
+                        numberOfBitVectorMatches+=1
+                if query==read:
+                        numberOfTrueMatches+=1
+        queriesEvaluated+=1
+        print(queriesEvaluated)
+        #               print(read + ", " +readInd)
+        #break
+        #print(queryInd)
+print(numberOfBitVectorMatches)
+print(numberOfTrueMatches)
