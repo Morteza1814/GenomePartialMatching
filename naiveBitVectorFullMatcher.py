bcfb52dbe7774367c3c8ddd709928690adf1d91f
Morteza Baradaran <65571259+Morteza1814@users.noreply.github.com>
2022-06-21 19:46:46 -0400

    Update naiveBitVectorFullMatcher.py
    added time getter to the code

--- naiveBitVectorFullMatcher.py
+++ naiveBitVectorFullMatcher.py
@@ -1,34 +1,36 @@
 import sys
+import time
 
-KMER_LENGTH = 4
+KMER_LENGTH = 10
 
 
 readFile = open("/bigtemp/rgq5aw/naiveBitVectorData/sampledDataFromSRR.fa", "r")
 queryFile = open("/bigtemp/rgq5aw/naiveBitVectorData/sampledQueryDataFromSRR.fa", "r")
-
+#fo = open("/bigtemp/rgq5aw/naiveBitVectorData/kmerSize32FP.txt","a")
+start_time = time.time()
 
 reads = dict()
 key=''
 value=''
 while(True):
-        line = readFile.readline()
-        if 'chr' in line and key=='':
-                 key = line[1:len(line)]
+	line = readFile.readline()
+	if 'chr' in line and key=='':
+		 key = line[1:len(line)]
 
         if 'chr' not in line and len(line)!=0 and key!='':
                 value = line
 
-        if key!='' and value!='':
-                reads[key] = value
-                key=''
-                value=''
-
-        if not line:
-                break
+	if key!='' and value!='':	
+		reads[key] = value
+		key=''
+		value=''
+	
+	if not line:
+		break
 
 i=0
-for value in reads:
-        i+=1
+for value in reads: 
+	i+=1
 print(i)
 
 queries = dict()
@@ -44,15 +46,15 @@ while(True):
 
         if key!='' and value!='':
                 queries[key] = value
-                key=''
-                value=''
+		key=''
+		value=''
 
         if not line:
                 break
 
 i=0
 for key in queries:
-        i+=1
+	i+=1
 print(i)
 
 readFile.close()
@@ -62,23 +64,26 @@ numberOfBitVectorMatches=0
 numberOfTrueMatches=0
 queriesEvaluated=0
 for queryInd,query in queries.items():
-        #print("query=\n" + query)
-        for readInd, read in reads.items():
-                i=0
-                numberOfKmersFound=0
-                while i<(len(query)-KMER_LENGTH):
-                        kmer = query[i:i+KMER_LENGTH]
-                        if kmer in read:
-                                numberOfKmersFound+=1
-                        i+=1
-                if numberOfKmersFound==((len(query)-1)-KMER_LENGTH+1):
-                        numberOfBitVectorMatches+=1
-                if query==read:
-                        numberOfTrueMatches+=1
-        queriesEvaluated+=1
-        print(queriesEvaluated)
-        #               print(read + ", " +readInd)
-        #break
-        #print(queryInd)
+	#print("query=\n" + query)
+	for readInd, read in reads.items():
+		i=0
+		numberOfKmersFound=0
+		while i<(len(query)-KMER_LENGTH):
+			kmer = query[i:i+KMER_LENGTH]
+			if kmer in read:
+				numberOfKmersFound+=1
+			i+=1
+		if numberOfKmersFound==((len(query)-1)-KMER_LENGTH+1):
+			numberOfBitVectorMatches+=1
+		if query==read:
+			numberOfTrueMatches+=1
+		#if numberOfKmersFound==((len(query)-1)-KMER_LENGTH+1) and query!=read:
+			#fo.write("-------------------------")
+			#fo.write("query" + query)
+			#fo.write("read" + read)
+	queriesEvaluated+=1
+	print(queriesEvaluated)
+
+print("--- %s seconds ---" % (time.time() - start_time))
 print(numberOfBitVectorMatches)
 print(numberOfTrueMatches)
