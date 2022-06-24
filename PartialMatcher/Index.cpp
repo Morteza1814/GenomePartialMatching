9b80717a62f99218555a8bb4a3b506fee3013619
Morteza Baradaran <65571259+Morteza1814@users.noreply.github.com>
2022-06-24 19:36:20 -0400

    Update Index.cpp
    add getter and setter

--- PartialMatcher/Index.cpp
+++ PartialMatcher/Index.cpp
@@ -43,33 +43,65 @@ unsigned long getIndexOfKmerInDictionary(const char* kmer)
     return kmerInBits;
 }
 
+void setBitVector(const char* kmer, unsigned char* bitVector, int kmerPos)
+{
+    // printf("kmer = %s \t", kmer);
+    int indexOfKmer = getIndexOfKmerInDictionary(kmer);
+    // cout << indexOfKmer << endl;
+    int indexInBitVector = indexOfKmer / 2;
+    int firstOrSecondHalf = indexOfKmer % 2;
+    unsigned int kmerRegionBitPattern = (unsigned int)pow(2, (kmerPos/(CONTIG_SIZE/NUMBER_OF_REGIONS)));
+    bitVector[indexInBitVector] |= (unsigned char)(((kmerRegionBitPattern & 0x07) << (firstOrSecondHalf*NUMBER_OF_BITS_FOR_EACH_KMER)));
+    // cout << kmerRegionBitPattern << endl;
+    // cout << indexInBitVector << endl;
+    // cout << (unsigned int)bitVector[indexInBitVector] << endl;
+}
+
+int getBitVector(const char* kmer, unsigned char* bitVector, int kmerPos)
+{
+    printf("kmer = %s \t", kmer);
+    int indexOfKmer = getIndexOfKmerInDictionary(kmer);
+    cout << indexOfKmer << endl;
+    int indexInBitVector = indexOfKmer / 2;
+    int firstOrSecondHalf = indexOfKmer % 2;
+    return (unsigned char)((bitVector[indexInBitVector] >> (firstOrSecondHalf*NUMBER_OF_BITS_FOR_EACH_KMER)) & 0x07);
+}
+
 void kmerizeAndMakeBitVector(const char* contig, unsigned char* bitVector)
 {
     char kmer[KMER_SIZE];
-    int indexOfKmer = 0;
-    int indexInBitVector=0;
-    int firstOrSecondHalf=0;
-    for(int i=0 ; i<(strlen(contig)-KMER_SIZE+1) ; i++)
+    if(strlen(contig) != CONTIG_SIZE)
+    {
+        cout << "Error : Contig size :" << strlen(contig) << " is not vlaid!" << endl;
+    }
+
+    for(int i=0 ; i <= CONTIG_SIZE-KMER_SIZE ; i++)
     {
         strncpy(kmer, contig+i, KMER_SIZE);
         kmer[KMER_SIZE]='\0';
-        printf("kmer = %s \t", kmer);
-        indexOfKmer = getIndexOfKmerInDictionary(kmer);
-        cout << indexOfKmer << endl;
-        indexInBitVector = indexOfKmer / 2;
-        firstOrSecondHalf = indexOfKmer % 2;
-        unsigned char tmp;
-        unsigned int kmerRegionBitPattern = (unsigned int)pow(2,(i+(KMER_SIZE/2))/(strlen(contig)/NUMBER_OF_REGIONS));
-        bitVector[indexInBitVector] |= (unsigned char)(((kmerRegionBitPattern & 0x0f) << (firstOrSecondHalf*NUMBER_OF_BITS_FOR_EACH_KMER)));
-        // cout << kmerRegionBitPattern << endl;
-        // cout << indexInBitVector << endl;
-        // cout << (unsigned int)bitVector[indexInBitVector] << endl;
+        setBitVector(kmer, bitVector, i+(KMER_SIZE/2));
+    }
+}
+
+void kmerizeAndReadBitVector(const char* contig, unsigned char* bitVector)
+{
+    char kmer[KMER_SIZE];
+    if(strlen(contig) != CONTIG_SIZE)
+    {
+        cout << "Error : Contig size :" << strlen(contig) << " is not vlaid!" << endl;
+    }
+
+    for(int i=0 ; i <= CONTIG_SIZE-KMER_SIZE ; i++)
+    {
+        strncpy(kmer, contig+i, KMER_SIZE);
+        kmer[KMER_SIZE]='\0';
+        cout << "i = " << i <<"\t : " << getBitVector(kmer, bitVector, i+(KMER_SIZE/2)) << endl;
     }
 }
 
 int main()
 {
-    char contig[15];
+    char contig[150];
     unsigned char bitVector[NUMBER_OF_BIT_VECTOR_BYTES];
 
     for(int i=0 ; i<NUMBER_OF_BIT_VECTOR_BYTES ; i++)
@@ -79,8 +111,9 @@ int main()
     cout << "kmer size is : " << KMER_SIZE << endl;
     cout << "number of bit vector bytes is : " << NUMBER_OF_BIT_VECTOR_BYTES << endl;
     
-    strcpy(contig, "AAAACAAAAGATTTT");
+    strcpy(contig, "GCCCTGATGAATTACCTCGTCTTTTCTCATATAACATGTCCTGGGAAGCCACAACATTGTGGTAAAGCTGTTCAACTACCACCGATACCATAGCAAGATGCTCATCTAGACTGTGACGACAATTACATCGTGAGAGATTGTGCTCTAGGC");
     kmerizeAndMakeBitVector(contig, bitVector);
+    // kmerizeAndReadBitVector(contig, bitVector);
     // for(int i=0 ; i<NUMBER_OF_BIT_VECTOR_BYTES ; i++)
     // {
     //     cout << i << " = " << (unsigned int)bitVector[i] << endl;
